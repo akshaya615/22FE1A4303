2c5dc18f21de9d6baa35f34677940b75c2b3e981
akshaya615 <akkiakshaya2426@gmail.com>
2025-09-04 13:06:17 +0530

    Update middleware.js

--- Logger Middleware/logger/middleware.js
+++ Logger Middleware/logger/middleware.js
@@ -1,1 +1,15 @@
+// logger/middleware.js
+const Log = require("./logger");
+
+function requestLogger(req, res, next) {
+  Log("backend", "info", "api", `Incoming request: ${req.method} ${req.url}`);
+  next();
+}
+
+function errorLogger(err, req, res, next) {
+  Log("backend", "error", "api", err.message);
+  res.status(500).json({ error: "Internal Server Error" });
+}
+
+module.exports = { requestLogger, errorLogger };
 
